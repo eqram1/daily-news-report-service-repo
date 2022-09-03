86a8a2f8de50d9756bb0f5b6ad0fee788ef482ac
eqram1 <abmeqramhossain@gmail.com>
2022-09-03 19:08:03 +0600

    added card section

--- js/reports.js
+++ js/reports.js
@@ -1,13 +1,31 @@
-const loadReports = () => {
-    fetch('https://openapi.programming-hero.com/api/news/category/01')
-        .then(res => res.json())
-        .then(data => displayReports(data.reports));
+const loadReports = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayReports(data.data);
 }
 
 const displayReports = reports => {
     const reportsContainer = document.getElementById('report-container');
     reports.forEach(report => {
-        console.log(report);
-    });
+        const reportDiv = document.createElement('div');
+        reportDiv.classList.add('rows');
+        reportDiv.innerHTML = `
+<div class="card h-100">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+`;
+        reportsContainer.appendChild(reportDiv);
+    })
 }
+
 loadReports();
+
